2e1dedad29d9b15b854b5fa60d6b8a1a8c11a48b
Ruin Lelouch <ruinhuan@outlook.com>
2018-04-04 20:01:26 +0800

    complete first version

--- src/test/java/com/wang/robot/RobotApplicationTests.java
+++ src/test/java/com/wang/robot/RobotApplicationTests.java
@@ -1,9 +1,13 @@
 package com.wang.robot;
 
+import com.wang.robot.bean.Result;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.awt.AWTException;
+import java.awt.Robot;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +15,18 @@ public class RobotApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		System.setProperty("java.awt.headless", "false");
+		Robot robot = null;
+		Result<String> result = new Result<>();
+		try {
+			robot = new Robot();
+			robot.keyPress(0xAE);
+			robot.keyRelease(0xAE);
+		} catch (AWTException e) {
+			e.printStackTrace();
+			result.fail();
+			result.setMsg("catch exception");
+		}
 	}
 
 }
