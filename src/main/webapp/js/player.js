1d7d330ee5741ee2b16b631d45cb493760159b62
Ruin Lelouch <ruinhuan@outlook.com>
2018-04-05 00:12:16 +0800

    complete first version

--- src/main/webapp/js/player.js
+++ src/main/webapp/js/player.js
@@ -1,63 +1,72 @@
 function myAlert(msg){
-    //alert(msg);
+    alert(msg);
 }
 
-$(".btn_big_prev").click(function(){
-    $.get("/mediaControl?cmd=prevSong",function(data, status){
+function execCmd(cmd){
+    $.get("/mediaControl?cmd=" + cmd,function(data, status){
         myAlert("数据: " + data + "\n状态: " + status);
+    });
+}
+
+$(document).ready(function(){
+    $(".btn_big_prev").click(function(){
+        $.get("/mediaControl?cmd=prevSong",function(data, status){
+            myAlert("数据: " + data + "\n状态: " + status);
+        });
+    });
+
+    $(".btn_big_play").click(function(){
+        $.get("/mediaControl?cmd=playOrPause",function(data, status){
+            myAlert("数据: " + data + "\n状态: " + status);
+        });
+    });
+
+    $(".btn_big_next").click(function(){
+        $.get("/mediaControl?cmd=nextSong",function(data, status){
+            myAlert("数据: " + data + "\n状态: " + status);
+        });
+    });
+
+    $("#increaseVolume").click(function(){
+        $.get("/mediaControl?cmd=volumeUp",function(data, status){
+            myAlert("数据: " + data + "\n状态: " + status);
+        });
+    });
+
+    $("#decreaseVolume").click(function(){
+        $.get("/mediaControl?cmd=volumeDown",function(data, status){
+            myAlert("数据: " + data + "\n状态: " + status);
+        });
+    });
+
+    $("#muteVolume").click(function(){
+        $.get("/mediaControl?cmd=volumeMute",function(data, status){
+            myAlert("数据: " + data + "\n状态: " + status);
+        });
+    });
+
+    $(".btn_volume_mute").click(function(){
+        $.get("/mediaControl?cmd=volumeMute",function(data, status){
+            myAlert("数据: " + data + "\n状态: " + status);
+        });
+    });
+
+    $(".btn_stop").click(function(){
+        $.get("/mediaControl?cmd=stop",function(data, status){
+            myAlert("数据: " + data + "\n状态: " + status);
+        });
+    });
+
+    $(".btn_shutdown").click(function(){
+        $.get("/systemControl?cmd=shutdown",function(data, status){
+            myAlert("数据: " + data + "\n状态: " + status);
+        });
+    });
+
+    $(".btn_cancel").click(function(){
+        $.get("/systemControl?cmd=cancel",function(data, status){
+            myAlert("数据: " + data + "\n状态: " + status);
+        });
     });
 });
 
-$(".btn_big_play").click(function(){
-    $.get("/mediaControl?cmd=playOrPause",function(data, status){
-        myAlert("数据: " + data + "\n状态: " + status);
-    });
-});
-
-$(".btn_big_next").click(function(){
-    $.get("/mediaControl?cmd=nextSong",function(data, status){
-        myAlert("数据: " + data + "\n状态: " + status);
-    });
-});
-
-$("#increaseVolume").click(function(){
-    $.get("/mediaControl?cmd=volumeUp",function(data, status){
-        myAlert("数据: " + data + "\n状态: " + status);
-    });
-});
-
-$("#decreaseVolume").click(function(){
-    $.get("/mediaControl?cmd=volumeDown",function(data, status){
-        myAlert("数据: " + data + "\n状态: " + status);
-    });
-});
-
-$("#muteVolume").click(function(){
-    $.get("/mediaControl?cmd=volumeMute",function(data, status){
-        myAlert("数据: " + data + "\n状态: " + status);
-    });
-});
-
-$(".btn_volume_mute").click(function(){
-    $.get("/mediaControl?cmd=volumeMute",function(data, status){
-        myAlert("数据: " + data + "\n状态: " + status);
-    });
-});
-
-$(".btn_stop").click(function(){
-    $.get("/mediaControl?cmd=stop",function(data, status){
-        myAlert("数据: " + data + "\n状态: " + status);
-    });
-});
-
-$(".btn_shutdown").click(function(){
-    $.get("/systemControl?cmd=shutdown",function(data, status){
-        myAlert("数据: " + data + "\n状态: " + status);
-    });
-});
-
-$(".btn_cancel").click(function(){
-    $.get("/systemControl?cmd=cancel",function(data, status){
-        myAlert("数据: " + data + "\n状态: " + status);
-    });
-});
